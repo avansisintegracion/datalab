0d5afbfca345ee54ec93df0fe1c4bb94dc142b5e
Cristian <cperez@kernix.com>
2017-01-20 17:51:16 +0100

    Add complete annotations for train set

--- proj-kaggle-fishes/src/data/BoundingBoxDownload.py
+++ proj-kaggle-fishes/src/data/BoundingBoxDownload.py
@@ -5,6 +5,8 @@ import os
 import urllib2
 
 LABELS_DIR = '../../data/external/labels/'
+# Labels are not complete, annos instead have the figure for all the fish in train set
+ANNOS_DIR = '../../data/external/annos/'
 
 # Links to labels produced by Nathaniel Shimoni, thanks for the great work!
 LABELS_LINKS = [
@@ -16,15 +18,25 @@ LABELS_LINKS = [
     'https://www.kaggle.com/blobs/download/forum-message-attachment-files/5378/alb_labels.json',
 ]
 
-def download_labels():
-    if not os.path.isdir(LABELS_DIR):
-        os.mkdir(LABELS_DIR)
-    for link in LABELS_LINKS:
+ANNOS_LINKS = [
+        'https://www.kaggle.com/blobs/download/forum-message-attachment-files/5458/bet_labels.json',
+        'https://www.kaggle.com/blobs/download/forum-message-attachment-files/5459/shark_labels.json',
+        'https://www.kaggle.com/blobs/download/forum-message-attachment-files/5460/dol_labels.json',
+        'https://www.kaggle.com/blobs/download/forum-message-attachment-files/5461/yft_labels.json',
+        'https://www.kaggle.com/blobs/download/forum-message-attachment-files/5462/alb_labels.json',
+        'https://www.kaggle.com/blobs/download/forum-message-attachment-files/5463/lag_labels.json',
+]
+
+def download_labels(fichier, httplink):
+    if not os.path.isdir(fichier):
+        os.mkdir(fichier)
+    for link in httplink:
         label_filename = link.split('/')[-1]
         print("Downloading " + label_filename)
         f = urllib2.urlopen(link)
-        with open(LABELS_DIR + label_filename, 'wb') as local_file:
+        with open(fichier + label_filename, 'wb') as local_file:
             local_file.write(f.read())
 
 if __name__ == '__main__':
-    download_labels()
+    download_labels(LABELS_DIR, LABELS_LINKS)
+    download_labels(ANNOS_DIR, ANNOS_LINKS)
